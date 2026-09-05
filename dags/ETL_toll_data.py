3a61cbcd6e8c6e13ddeba0322e309c2dfd85fbc3
Bayu Wicaksono <bayuwcksn01@gmail.com>
2026-09-05 16:26:27 +0700

    feat: complete Airflow ETL pipeline tasks

--- dags/ETL_toll_data.py
+++ dags/ETL_toll_data.py
@@ -18,5 +18,44 @@ dag = DAG(
     dag_id='ETL_toll_data',
     default_args=default_args,
     description='Apache Airflow Final Assignment',
-    schedule_interval='@daily'
+    schedule='@daily'
 )
+
+
+unzip_data = BashOperator(
+    task_id='unzip_data',
+    bash_command='tar -xvzf /opt/airflow/data/raw/tolldata.tgz -C /opt/airflow/data/raw',
+    dag=dag
+)
+
+extract_data_from_csv = BashOperator(
+    task_id='extract_data_from_csv',
+    bash_command='cut -d, -f1-4 /opt/airflow/data/raw/vehicle-data.csv > /opt/airflow/data/processed/csv_data.csv',
+    dag=dag
+)
+
+extract_data_from_tsv = BashOperator(
+    task_id='extract_data_from_tsv',
+    bash_command="cut -f5-7 /opt/airflow/data/raw/tollplaza-data.tsv | tr '\\t' ',' | tr -d '\\r' > /opt/airflow/data/processed/tsv_data.csv",
+    dag=dag
+)
+
+extract_data_from_fixed_width = BashOperator(
+    task_id='extract_data_from_fixed_width',
+    bash_command="awk '{print substr($0,59,3) \",\" substr($0,63,5)}' /opt/airflow/data/raw/payment-data.txt > /opt/airflow/data/processed/fixed_width_data.csv",
+    dag=dag
+)
+
+consolidate_data = BashOperator(
+    task_id='consolidate_data',
+    bash_command='paste -d, /opt/airflow/data/processed/csv_data.csv /opt/airflow/data/processed/tsv_data.csv /opt/airflow/data/processed/fixed_width_data.csv > /opt/airflow/data/processed/extracted_data.csv',
+    dag=dag
+)
+
+transform_data = BashOperator(
+    task_id='transform_data',
+    bash_command="tr '[:lower:]' '[:upper:]' < /opt/airflow/data/processed/extracted_data.csv > /opt/airflow/data/processed/transformed_data.csv",
+    dag=dag
+)
+
+unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data >> transform_data
